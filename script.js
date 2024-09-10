1e455beba9836a291c74b2cdd68517fdf8bbdaea
Luna <62033805+Luna5ama@users.noreply.github.com>
2024-09-10 18:15:57 -0400

    Updated comments in script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
     // Initialize DataTables
     const table = $('#publicationTable').DataTable({
-        scrollX: false, // Enable horizontal scrolling
+        scrollX: false, // Disable horizontal scrolling
         scrollY: 1000,   // Set a fixed height for vertical scrolling (adjust as needed)
         scrollCollapse: true, // Enable scrollbars when needed
-        paging: true, // Disable pagination
-        pageLength: 50, //
+        paging: true, // Enable pagination
+        pageLength: 50, // Number of entries per page
         ordering: true, // Enable sorting
-        order: [[4, 'desc']]
+        order: [[4, 'desc']] // Sort by the 5th column (year) in descending order
     });
 });
