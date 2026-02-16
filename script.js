fee3ea2070322ad146032e3038504a75a5cf947c
Adam M. Wilson <900623+adammwilson@users.noreply.github.com>
2026-02-16 10:54:33 -0500

    Adjust DataTable scrolling and pagination settings

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
     // Initialize DataTables
     const table = $('#publicationTable').DataTable({
         scrollX: false, // Disable horizontal scrolling
-        scrollY: 1000,   // Set a fixed height for vertical scrolling (adjust as needed)
+        scrollY: 3000,   // Set a fixed height for vertical scrolling (adjust as needed)
         scrollCollapse: true, // Enable scrollbars when needed
         paging: true, // Enable pagination
-        pageLength: 50, // Number of entries per page
+        pageLength: 120, // Number of entries per page
         ordering: true, // Enable sorting
         order: [[4, 'desc']] // Sort by the 5th column (year) in descending order
     });
