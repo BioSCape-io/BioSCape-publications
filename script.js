e14770ca7aa11f3096bc151077215a48c4ba11c4
Adam M. Wilson <900623+adammwilson@users.noreply.github.com>
2026-03-11 12:19:10 -0400

    Remove scrollCollapse option and adjust page length for publication table

--- script.js
+++ script.js
@@ -3,9 +3,8 @@ $(document).ready(function() {
     const table = $('#publicationTable').DataTable({
         scrollX: false, // Disable horizontal scrolling
         scrollY: false,   // Set a fixed height for vertical scrolling (adjust as needed)
-        scrollCollapse: false, // Enable scrollbars when needed
         paging: false, // Enable pagination
-        pageLength: 200, // Number of entries per page
+        pageLength: 250, // Number of entries per page
         ordering: true, // Enable sorting
         order: [[4, 'desc']] // Sort by the 5th column (year) in descending order
     });
